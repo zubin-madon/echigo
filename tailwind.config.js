01c32fe2f29390ed5d5a8b4079dba9ca4ca10286
zubin-madon <madon.zubin@gmail.com>
2022-05-11 11:58:03 +0530

    landing page start

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'echigo-pink': '#cc0066'
+      }
+    },
+  },
+  plugins: [],
+}
